f7f505a986019a1f9d810bece9fa596655a66814
Moshe Maizels <moshemaizels@gmail.com>
2017-09-25 13:45:55 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,8 @@ def turn_count(board)
   counter = 0
   board.each do |board_spot|
     if board_spot == "X" || board_spot =="O"
-      counter+=1
+      counter += 1
     end
   end 
+  return counter
 end
